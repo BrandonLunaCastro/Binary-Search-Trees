fc094d77c4a165a840594f00f0b34c0c2e9f3b43
BrandonLunaCastro <blunacastro2003@gmail.com>
2023-10-09 20:18:36 -0300

    fixed: add class Node to generate nodes

--- node.js
+++ node.js
@@ -1,7 +1,7 @@
 export default class Node {
-  constructor (data, leftChild, rightChild) {
-    this.data = data
-    this.leftChild = leftChild
-    this.rightChild = rightChild
+  constructor (data, leftChild = null, rightChild = null) {
+    this.data = data;
+    this.leftChild = leftChild;
+    this.rightChild = rightChild;
   }
 }
